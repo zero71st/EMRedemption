236e0b24fcc0b8af5f69b467a03bbb203478587f
kasem <kasem.w@dzcard.com>
2018-11-05 08:40:01 +0700

    Re-create migrations.

--- EMRedemption/Data/Migrations/25611105013816_Re-genereate migration.Designer.cs
+++ EMRedemption/Data/Migrations/25611105013816_Re-genereate migration.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace EMRedemption.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("25611105013816_Re-genereate migration")]
+    partial class Regenereatemigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
