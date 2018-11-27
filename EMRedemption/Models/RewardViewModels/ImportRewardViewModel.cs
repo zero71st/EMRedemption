7834be725a256281cde9e1aa947e38309868ec94
kasem <kasem.w@dzcard.com>
2018-11-27 09:19:47 +0700

    Added show and hide to load,save,cancel.

--- EMRedemption/Models/RewardViewModels/ImportRewardViewModel.cs
+++ EMRedemption/Models/RewardViewModels/ImportRewardViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,12 +11,14 @@ namespace EMRedemption.Models.RewardViewModels
     public class ImportRewardViewModel
     {
         public int RewardTypeId { get; set; }
+        [DisplayName("Reward Type")]
         public SelectList RewardTypes { get; set; }
+        [DisplayName("Lot No.")]
         public DateTime LotDate { get; set; }
      
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Unlock file Password")]
+        [Display(Name = "Decrypt Password")]
         public string Password { get; set; }
         public string Description { get; set; }
     }
